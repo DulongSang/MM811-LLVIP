76a5e2900a764c3409cc1e09e93af0d9996ca3d7
Dulong Sang <dulong@ualberta.ca>
2023-12-17 22:01:38 -0700

    feat: update YOLOv8 training with more arguments

--- yolov8/train.py
+++ yolov8/train.py
@@ -5,28 +5,42 @@ from ultralytics import YOLO
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Object Detection")
-    parser.add_argument("--load-pretrained", type=str, default=None, help="Path to the pretrained detection model checkpoint")
+    parser.add_argument("--checkpoint", type=str, default="yolov8n.pt", help="Path to detection model checkpoint")
+    parser.add_argument("--resume", action="store_true", help="resume training from last checkpoint")
+    parser.add_argument("--project", type=str, default="runs/train", help="Path to save the model")
     parser.add_argument("--data", type=str, default="LLVIP.yaml", help="Path to data config file")
     parser.add_argument("--device", type=str, default="cuda", help="Device to run the model on, either cpu or cuda (0, 1, 2, ...)")
     parser.add_argument("--epochs", type=int, default=10, help="Number of epochs to train the model")
-    parser.add_argument("--batch-size", type=int, default=32, help="Batch size")
+    parser.add_argument("--batch-size", type=int, default=32, help="Batch size, -1 for AutoBatch")
+    parser.add_argument("--lr", type=float, default=0.001, help="Learning rate")
+    parser.add_argument("--lrf", type=float, default=0.1, help="final learning rate (lr * lrf)")
+    parser.add_argument("--momentum", type=float, default=0.9, help="Momentum")
+    parser.add_argument("--weight-decay", type=float, default=0.0005, help="Weight decay")
+    parser.add_argument("--optimizer", type=str, default="SGD", choices=['SGD', 'Adam', 'Adamax', 'AdamW', 'NAdam', 'RAdam', 'RMSProp', 'auto'], help="learning optimizer")
     return parser.parse_args()
 
 
 def main():
     args = parse_args()
-    model = YOLO(args.data, task='detect')
+    model = YOLO(args.checkpoint, task='detect')
 
     # device
     device = torch.device(args.device)
     model.to(device)
-
-    # load pretrained weights
-    if args.load_pretrained:
-        model.load(args.load_pretrained)
     
     # train
-    model.train(data=args.data, epochs=args.epochs, batch_size=args.batch_size)
+    model.train(
+        data=args.data,
+        epochs=args.epochs,
+        batch=args.batch_size,
+        lr0=args.lr,
+        lrf=args.lrf,
+        momentum=args.momentum,
+        weight_decay=args.weight_decay,
+        optimizer=args.optimizer,
+        project=args.project,
+        resume=args.resume,
+    )
 
 
 if __name__ == "__main__":
